74ab0c83de3a96f59442ce602ecb43f3fbaad00e
Oshima Hiroki <135520636+hakonam1@users.noreply.github.com>
2023-06-12 00:15:56 +0900

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,61 +1,60 @@
+// script.js
 const socket = io();
-
-// チャットログの表示
 const chatLogContainer = document.getElementById('chatLog');
-function displayChatLog(chatLog) {
-  chatLogContainer.innerHTML = '';
 
-  chatLog.forEach((message) => {
-    const messageElement = document.createElement('div');
-    const timestamp = luxon.DateTime.fromFormat(message.timestamp, 'yyMMddHHmmss', { zone: 'Asia/Tokyo' });
-
-    const formattedTimestamp = `${String(timestamp.hour).padStart(2, '0')}:${String(timestamp.minute).padStart(2, '0')}:${String(timestamp.second).padStart(2, '0')}`;
-
-    messageElement.innerHTML = `
-      <div class="message">
-        <div class="message-content">${message.text}</div>
-        <div class="message-timestamp">${formattedTimestamp}</div>
-      </div>
-    `;
-
-    chatLogContainer.appendChild(messageElement);
-  });
+function createMessageElement(message) {
+  const messageElement = document.createElement('div');
+  const timestamp = luxon.DateTime.fromFormat(message.timestamp, 'yyMMddHHmmss', { zone: 'Asia/Tokyo' });
+  const formattedTimestamp = `${String(timestamp.hour).padStart(2, '0')}:${String(timestamp.minute).padStart(2, '0')}:${String(timestamp.second).padStart(2, '0')}`;
+  messageElement.innerHTML = `
+    <div class="message ${message.isOwnMessage ? 'own-message' : ''}">
+      <div class="message-content">${message.text}</div>
+      <div class="message-timestamp">${formattedTimestamp}</div>
+    </div>
+  `;
+  return messageElement;
 }
 
+function displayChatLog(chatLog) {
+  chatLogContainer.innerHTML = '';
+  chatLog.forEach((message) => {
+    chatLogContainer.appendChild(createMessageElement(message));
+  });
+  chatLogContainer.scrollTop = chatLogContainer.scrollHeight;
+}
 
-// メッセージ送信
-const messageForm = document.getElementById('messageForm');
-const messageInput = document.getElementById('messageInput');
-
-messageForm.addEventListener('submit', (e) => {
-  e.preventDefault();
+function sendMessage() {
   const message = messageInput.value.trim();
   if (message !== '') {
     socket.emit('message', message);
     messageInput.value = '';
+    chatLogContainer.appendChild(createMessageElement({
+      text: message,
+      timestamp: luxon.DateTime.local().setZone('Asia/Tokyo').toFormat('yyMMddHHmmss'),
+      isOwnMessage: true
+    }));
+    chatLogContainer.scrollTop = chatLogContainer.scrollHeight;
   }
-});
+}
 
-// Discord Webhookの設定
-const webhookForm = document.getElementById('webhookForm');
-const webhookInput = document.getElementById('webhookInput');
-const webhookBtn = document.getElementById('webhookBtn');
-
-webhookForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-});
-
-webhookBtn.addEventListener('click', () => {
+function setWebhook() {
   const webhookURL = webhookInput.value.trim();
   if (webhookURL !== '') {
     socket.emit('setWebhook', webhookURL);
     webhookInput.value = '';
   }
+}
+
+// メッセージ送信
+const messageForm = document.getElementById('messageForm');
+const messageInput = document.getElementById('messageInput');
+messageForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  sendMessage();
 });
 
 // 接続数の表示
 const userCountElement = document.getElementById('userCount');
-
 socket.on('userCount', (userCount) => {
   userCountElement.textContent = `現在の接続数: ${userCount}`;
 });
@@ -63,4 +62,12 @@ socket.on('userCount', (userCount) => {
 // チャットログを受信したときの処理
 socket.on('chatLog', (chatLog) => {
   displayChatLog(chatLog);
+});
+
+// Discord Webhookの設定
+const webhookForm = document.getElementById('webhookForm');
+const webhookInput = document.getElementById('webhookInput');
+webhookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  setWebhook();
 });
